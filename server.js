c5e1e39666be037992e3db79a55f4f44f2bdbe62
Gowthami Palle <gowthamipalle@gmail.com>
2020-04-10 00:15:46 -0400

    added server.js

--- server.js
+++ server.js
@@ -466,10 +466,10 @@ app.get('/api/rating/:recipeId/', function (req, res, next) {
 });
 
 if (process.env.NODE_ENV == "production") {
-  app.use(express.static(path.join(__dirname, "build")));
+  app.use(express.static(path.join(__dirname, "recipes/build")));
 
   app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "build", "index.html"));
+    res.sendFile(path.join(__dirname, "recipes/build/index.html"));
   });
 }
 
